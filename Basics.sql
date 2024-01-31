9af22e7329020d11ae07abd6352faaf639f9136b
MarothiGore <119786992+MarothiGore@users.noreply.github.com>
2024-01-31 12:09:50 +0530

    Add files via upload

---
+++ Basics.sql
@@ -0,0 +1,30 @@
+##table of student
+ create table 
+ student(
+ stud_ID integer,
+ sname char(30),age integer,
+ course char(30));
+
+   insert into student values(
+ 1,"Tom",32,"MYSql");
+ select * from student;
+ insert into student values( 2,
+ "Merry",23,"Data Analysis");
+ insert into student values( 2,
+ "joe",23,"Data Science");
+ 
+ select * from  student;
+ 
+ 
+ insert into emp values( 1,
+ "ram",56,"IT");
+ select * from emp;
+
+
+ 
+  create table emp2(
+ emp_ID integer,sname char(30),
+ age integer,
+ dept char(30));
+ insert into emp3 values(1,"Syed",5,"IT");
+ select * from  emp2;empempempstudent
