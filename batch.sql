95032ed5e3f42f57dec83aab43f7e85efb10baa6
MarothiGore <119786992+MarothiGore@users.noreply.github.com>
2024-02-02 12:53:47 +0530

    Add files via upload

---
+++ batch.sql
@@ -0,0 +1,18 @@
+create database batch34;
+use batch34;
+show  tables;
+ create table students 
+ ( sid integer,
+ sname char(30),
+ age integer, 
+ course char(20));
+ desc students; 
+ 
+
+ 
+ desc students;
+ show table status;
+  table   students;
+ describe  students;
+  insert into students values(1,'Thomas',25,'Mysql');
+ insert into students values(2,'Vijay',34,'Data Science');
---
+++ create.sql
@@ -0,0 +1,3 @@
+create table authors (Author INTEGER primary key,Name Varchar(70));
+desc authors;
+insert  authors values(1,'J K Rowling');
