e3081411052e34386348c223c45df730a9aee9e4
MarothiGore <119786992+MarothiGore@users.noreply.github.com>
2024-02-04 21:40:48 +0530

    Add files via upload

---
+++ DateTime.sql
@@ -0,0 +1,7 @@
+ create  table student (sid integer,sname char(30),age integer,course char(30));
+ insert into student values(1,'Thomas',30,'Data Analystics');
+insert into student values(2,'Mary',34,'Data Sciecne');
+desc student;
+
+
+create    table  patients (pid integer,pname varchar(30),dob date,toa datetime);
---
+++ emp2.sql
@@ -0,0 +1,7 @@
+ create table emp2(
+ emp_ID integer,sname char(30),
+ age integer,
+ dept char(30));
+ insert into emp3 values(1,"Syed",5,"IT");
+ select * from  emp2;
+ 
